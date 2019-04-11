7977389ffae3ad4c58c4491aa622016eae4b1f22
bhawani85 <kandari2007engg@gmail.com>
2019-04-12 00:28:24 +0530

    first git commit for selenium

---
+++ src/main/java/DemoMavenProject/DemoMaven/Test1.java
@@ -0,0 +1,11 @@
+package DemoMavenProject.DemoMaven;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Print the hello");
+	}
+
+}
---
+++ src/test/java/DemoMavenProject/DemoMaven/Test2.java
@@ -0,0 +1,12 @@
+package DemoMavenProject.DemoMaven;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("GO to Git Repository");
+	}
+
+}
